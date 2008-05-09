ee4f4c899b503253efc7ea8772f50f430eb200ad
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-09 10:26:24 +0000

    Fix a typo in server config template, add comments.
    git-svn-id: http://smob.googlecode.com/svn/trunk@3 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- server/config_dist.php
+++ server/config_dist.php
@@ -2,14 +2,16 @@
 
 include_once(dirname(__FILE__).'/lib/ARC2/ARC2.php');
 
+// SQL database configuration for storing the postings:
 $arc_config = array(
-  'db_host' => 'localhost' /* optional, default is localhost */
+  'db_host' => 'localhost', /* optional, default is localhost */
   'db_name' => 'smob',
   'db_user' => '****',
   'db_pwd' => '****',
   'store_name' => 'smob',
 );
 
+// Google Maps API key from http://code.google.com/apis/maps/signup.html
 $gmap_key = '';
 
 ?>
