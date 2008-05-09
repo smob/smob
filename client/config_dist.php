3284890ecfc5585ba69507d9c8c7a88bc94966dc
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-09 12:00:31 +0000

    Add comments to client config template
    git-svn-id: http://smob.googlecode.com/svn/trunk@4 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- client/config_dist.php
+++ client/config_dist.php
@@ -1,14 +1,17 @@
 <?php
 
-// Servers where you want to publish your data
-// Use 'server' => 'key' format
+// Addresses of the servers where you want to publish your data
+// Use 'server' => 'apikey' format (API key can be empty for now)
 $servers = array(
-  'http://apassant.net/home/2008/03/smob/server'=>''
+  'http://apassant.net/home/2008/03/smob/server'=>'',
 );
 
-// Edit the following lines
-$foaf_uri = 'http://apassant.net/alex';
+// The following settings configure your identity settings
+// FOAF URL is the http location of your FOAF profile file
 $foaf_url = 'http://apassant.net/foaf.rdf';
+// FOAF URI identifies you and should be described in your FOAF profile
+$foaf_uri = 'http://apassant.net/alex';
+// SIOC nick is used as your microblogger user name
 $sioc_nick = 'alex';
 
 ?>
