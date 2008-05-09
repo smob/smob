15b54349167b90b020199af7f407206ee64240ae
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-09 12:01:55 +0000

    Don't depend on SCRIPT_URI. Output progress information.
    git-svn-id: http://smob.googlecode.com/svn/trunk@5 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- client/publish/index.php
+++ client/publish/index.php
@@ -19,7 +19,9 @@ function send_data($url, $server) {
 
 if($content=$_POST['content']) {
   $ts = date('c');
-  $root =  substr(dirname($_SERVER['SCRIPT_URI']), 0, -strlen('/publish'));
+  // SCRIPT_URI isn't present on all servers, so we do this instead:
+  $authority = "http://" . $_SERVER['HTTP_HOST'];
+  $root = $authority . dirname(dirname($_SERVER['SCRIPT_NAME'])); 
   $post = "$root/data/$ts";
   $ex = new SIOCExporter();
   $user = new SIOCUser($sioc_nick, "$root/user/$sioc_nick", 'name', 'mail', 'page', $foaf_uri, '', '', '', $foaf_url);
@@ -28,11 +30,19 @@ if($content=$_POST['content']) {
   $f = fopen(dirname(__FILE__)."/../data/$ts.rdf", 'w');
   fwrite($f, $rdf);
   fclose($f);
+  print "<ul>\n";
   foreach($_POST['servers'] as $k => $server) {
+    print "<li> Telling <a href='$server'>$server</a>";
+    print " about <a href='$post.rdf'>$post.rdf</a>...\n";
     send_data("$post.rdf", $server);
+    print " done. </li>\n";
+    print "<li> Telling <a href='$server'>$server</a>";
+    print " about <a href='$foaf_url'>$foaf_url</a>...\n";
     // The FOAF file should not be sent everytime - fix it
     send_data($foaf_url, $server);
+    print " done. </li>\n";
   }
+  print "</ul>\n";
 }
 
 ?>
