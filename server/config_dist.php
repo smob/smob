f247f80b1ec5568ad6af336fc8694fe27c3ebbaf
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-12 18:57:53 +0000

    Update server documentation
    git-svn-id: http://smob.googlecode.com/svn/trunk@27 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- server/config_dist.php
+++ server/config_dist.php
@@ -11,7 +11,9 @@ $arc_config = array(
   'store_name' => 'smob',
 );
 
-// Global authentication API key
+// Global authentication API key - set this to something hard to guess if
+// you want to stop publishers getting their postings aggregated unless
+// they provide it too.
 $auth_key = '';
 
 // Google Maps API key from http://code.google.com/apis/maps/signup.html
