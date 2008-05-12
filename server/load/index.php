1afd653b81aea802ffe76d2bcbb48b6d26d7f0fd
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-12 19:11:01 +0000

    Add error reporting to server/publish
    git-svn-id: http://smob.googlecode.com/svn/trunk@28 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- server/load/index.php
+++ server/load/index.php
@@ -8,9 +8,33 @@ if (!$store->isSetUp()) {
 }
 
 $data = $_GET['data'];
-if($data) {
-  if($auth_key && (!$_GET['key'] || $_GET['key'] != $auth_key)) die(); 
+if ($data) {
+  if ($_GET['key'] != $auth_key) {
+    header("HTTP/1.0 403 Forbidden");
+    header("Content-Type: text/html"); 
+    print "No correct API key given!\n";
+    exit;
+  }
   $rs = $store->query("LOAD <$data>");
+} else {
+  header("HTTP/1.0 404 Not found");
+  header("Content-Type: text/html"); 
+  print "No data URI to load given!\n";
+  exit;
 }
+
+if ($errs = $store->getErrors()) {
+  header("HTTP/1.0 500 Internal Server Error");
+  header("Content-Type: text/html"); 
+  print "<p>The following operation failed: ";
+  print htmlspecialchars("LOAD <$data>\n\n");
+  print "\n\n<ul>\n";
+  foreach ($errs as $err)
+    print "<li>" . htmlspecialchars($err) . "</li>\n";
+  print "</ul>";
+  exit;
+}
+
+print "Done."
 
 ?>
