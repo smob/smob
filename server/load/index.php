ed43f9917a1e99429ef97c0bc39da7bb041807dc
tuukka <Tuukka.Hastrup@iki.fi>
2008-05-12 22:35:41 +0000

    Streamline server/load a bit
    git-svn-id: http://smob.googlecode.com/svn/trunk@33 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- server/load/index.php
+++ server/load/index.php
@@ -11,24 +11,25 @@ if (!$store->isSetUp()) {
   $store->setUp();
 }
 
+$key = $_GET['key'];
 $data = $_GET['data'];
-if ($data) {
-  if ($_GET['key'] != $auth_key) {
-    header("HTTP/1.0 403 Forbidden");
-    header("Content-Type: text/html"); 
-    print "No correct API key given!\n";
-    exit;
-  }
 
-  $store->query("DELETE FROM <$data>"); // delete any old data first
-  $rs = $store->query("LOAD <$data> INTO <$data>");
+if ($key != $auth_key) {
+  header("HTTP/1.0 403 Forbidden");
+  header("Content-Type: text/html"); 
+  print "No correct API key given!\n";
+  exit;
+}
 
-} else {
+if (!$data) {
   header("HTTP/1.0 404 Not found");
   header("Content-Type: text/html"); 
   print "No data URI to load given!\n";
   exit;
 }
+
+$store->query("DELETE FROM <$data>"); // delete any old data first
+$rs = $store->query("LOAD <$data> INTO <$data>");
 
 if ($errs = $store->getErrors()) {
   header("HTTP/1.0 500 Internal Server Error");
