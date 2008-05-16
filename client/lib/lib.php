58977832a18c281f735eb36f7322370f5889c46c
terraces <apassant@gmail.com>
2008-05-16 20:52:18 +0000

    RDFa in client homepage
    git-svn-id: http://smob.googlecode.com/svn/trunk@44 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- client/lib/lib.php
+++ client/lib/lib.php
@@ -5,10 +5,20 @@ require_once(dirname(__FILE__)."/ARC2/ARC2.php");
 function smob_header() {
   global $sioc_nick;
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" 
+  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 
-<html>
-
-<head>
+<html
+  xmlns="http://www.w3.org/1999/xhtml" 
+  xmlns:dc="http://purl.org/dc/elements/1.1/"
+  xmlns:dcterms="http://purl.org/dc/terms/"
+  xmlns:foaf="http://xmlns.com/foaf/0.1/" 
+  xmlns:sioc="http://rdfs.org/sioc/ns#"
+  xmlns:sioct="http://rdfs.org/sioc/types#"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/"
+xml:lang="fr">
+ 
+<head profile="http://ns.inria.fr/grddl/rdfa/">
   <title>SMOB - <?php echo $sioc_nick; ?></title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
@@ -16,8 +26,8 @@ function smob_header() {
 <body>
 
 <ul id="menu">
-<li><a href="data">browse data</a></li>
-<li><a href="publish">publish</a></li/>
+  <li><a href="data">browse data</a></li>
+  <li><a href="publish">publish</a></li>
 </ul>
 
 <div id="main">
@@ -27,6 +37,9 @@ function smob_header() {
 
 function smob_footer() {
 ?>
+
+</div>
+
 </body>
 
 </html>
@@ -35,16 +48,16 @@ function smob_footer() {
 
 function show_networks() {
   global $servers, $twitter_user;
-  echo "<h1>My networks</h1>";
-  echo "<ul>";
+  echo "<h1>My networks</h1>\n\n";
+  echo "<ul>\n";
   foreach($servers as $server => $key) {
-    echo "<li><a href='$server'>$server</a></li>";
+    echo "  <li><a href='$server'>$server</a></li>\n";
   }
   if ($twitter_user) {
-    echo "<li>Twitter as " .
-       "<a href='http://twitter.com/$twitter_user'>$twitter_user</a>";
+    echo "  <li>Twitter as " .
+       "<a href='http://twitter.com/$twitter_user'>$twitter_user</a></li>\n";
   }
-  echo "</ul>";
+  echo "</ul>\n\n";
 }
 
 function show_post($id) {
@@ -60,15 +73,15 @@ function show_post($id) {
   $date = $datapost['http://purl.org/dc/terms/created'][0];
   $content = $datapost['http://rdfs.org/sioc/ns#content'][0];
   
-  echo '<div id="post">';
-  echo "<div id=\"content\">$content</div>";
-  echo "<div id=\"date\">$date</div>";
-  echo '</div>';
+  echo "<div class=\"post\" typeof=\"sioct:MicroblogPost\" about=\"$post\">\n";
+  echo "  <span class=\"content\" property=\"sioc:content\">$content</span>\n";
+  echo "  <span class=\"date\" property=\"dcterms:created\">$date</span>\n";
+  echo "</div>\n\n";
 }
 
 function show_posts($start=0, $limit=20) {
   $data = get_posts($start, $limit);
-  echo "<h1>Latest updates</h1>";
+  echo "<h1>Latest updates</h1>\n\n";
   foreach($data as $post) {
     show_post($post);
   }
@@ -85,4 +98,5 @@ function get_posts($start=0, $limit=20) {
 }
 
 ?>
+
 
