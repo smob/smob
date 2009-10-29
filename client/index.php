789d25368003d0ac8d98e0767eda98969bd61da3
tuukka <Tuukka.Hastrup@iki.fi>
2009-10-29 21:22:10 +0000

    $root needs to be defined in all scripts that are requested
    git-svn-id: http://smob.googlecode.com/svn/trunk@96 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- client/index.php
+++ client/index.php
@@ -1,5 +1,9 @@
 <?php 
 
+// SCRIPT_URI isn't present on all servers, so we do this instead:
+$authority = "http://" . $_SERVER['HTTP_HOST'];
+$root = $authority . dirname(dirname($_SERVER['SCRIPT_NAME'])); 
+
 require_once(dirname(__FILE__).'/../lib/smob/client.php'); 
 
 if(!file_exists(dirname(__FILE__)."/../config.php")) {
