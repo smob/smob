9e4ef2ea3d22d67f66ecaf993e88ec4b698998d0
terraces <apassant@gmail.com>
2009-10-29 23:55:03 +0000

    Removed suggest_uri.php and merged with pub.php
    git-svn-id: http://smob.googlecode.com/svn/trunk@104 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- client/publish/pub.php
+++ client/publish/pub.php
@@ -1,9 +1,12 @@
 <?php
 
-// SCRIPT_URI isn't present on all servers, so we do this instead:
+require_once(dirname(__FILE__)."/../../config.php");
+require_once(dirname(__FILE__)."/../../lib/smob/lib.php");
+
 $authority = "http://" . $_SERVER['HTTP_HOST'];
 $root = $authority . dirname(dirname(dirname($_SERVER['SCRIPT_NAME']))); 
 
+	
 function twitter_post($content, $user, $pass)  {
   $dest = 'http://twitter.com/statuses/update.xml';
   return curl_post($dest, $content, $user, $pass);
@@ -161,4 +164,154 @@ function publish($content) {
 }
 
 
+function get_wrappers($type) {
+	if ($handle = opendir(dirname(__FILE__)."/wrappers/$type")) {
+    	while (false !== ($file = readdir($handle))) {
+			if (substr($file, 0, 1) != '.') {
+				$services[] = $file;
+				require_once(dirname(__FILE__)."/wrappers/$type/$file/index.php");
+			}
+		}
+		closedir($handle);
+	}
+	return $services;
+}
+
+function find_uris($type, $term) {
+	global $wrappers;
+	foreach($wrappers[$type] as $w) {
+		$w = ucfirst($w).ucfirst($type).'Wrapper';
+		$x = new $w($term);
+		$uris[$w] = $x->get_uri();	
+	}
+	return $uris;
+}
+
+$content = $_GET['content'];
+$post = $_GET['post'];
+
+
+// In case we need to publish content
+if($content) {
+
+$wrappers['user'] = get_wrappers('user');	
+$wrappers['tag'] = get_wrappers('tag');	
+
+$ex = explode(' ', $content);
+
+foreach($ex as $e) {
+	if(substr($e, 0, 1) == '@') {
+		$user = substr($e, 1);
+		$users[$user] = find_uris('user', $user);
+	}
+	elseif(substr($e, 0, 1) == '#') {
+		$tag = substr($e, 1);
+		$tags[$tag] = find_uris('tag', $tag);
+	}
+}
+
+// @@TODO : keep mappings in the local store for automation
+
+
+// Publish the post
+$id = publish($content);
+
+// Output the results to let the user chose his URIs -- if there are some tags
+if($users || $tags) {
+print "<h2>LOD Integration</h2>";
+print "<form id='mappings-form'>";
+print "<input type='hidden' value='$id' id='post-id'>";
+foreach(array('tags' => $tags, 'users' => $users) as $type => $items) {
+if($items) {
+	print "<fieldset><legend>$type</legend>";
+	foreach($items as $item=>$wrapper) {
+		print "<fieldset><legend>$item</legend>";
+		foreach($wrapper as $wname => $uris) {
+			print "<fieldset><legend>$wname</legend>";
+			if($uris) {
+				foreach($uris as $name=>$uri) {
+					$val = "$type--$item--$uri";
+					print "<input type='checkbox' value='$val'/>$name ($uri)<br/>";
+				}
+			} else {
+				print "Nothing retrieved from this service<br/>";
+			}
+			print "</fieldset>";
+		}
+		print "</fieldset>";
+	}
+	print "</fieldset>";
+}
+}
+print "</form>";
+print '
+<script type="text/javascript">
+$(function() {
+	$("#mappings").click(function () {
+		mappings();
+	});
+});
+</script>
+
+<div id="smob-mappings" style="display: none;">
+	<em>Publishing content ...</em>
+</div>	<button id="mappings">Update mappings</button>
+';
+}
+}
+
+// In case we just need to update the mappings
+if($post) {
+	$checked = $_GET['checked'];
+	$unchecked = $_GET['unchecked'];
+	$ck = explode(' ', $checked);
+	foreach($ck as $c) {
+		$ckl = explode('--', $c);
+		if($ckl[0] == 'users') {
+			$user = $ckl[1];
+			$uri = $ckl[2];
+			// Update with sioc:xxx
+			$triples[] = array(uri($post), "sioc:topic", uri($uri));
+			$triples[] = array(uri($uri), "sioc:name", literal($user));
+			
+		}
+		elseif($ckl[0] == 'tags') {
+			$tag = $ckl[1];
+			$uri = $ckl[2];
+			// Update with MOAT / commonTag
+			$tagging = 'http://example.org/tagging/'.uniqid();
+			$triples[] = array(uri($tagging), "a", "tags:RestrictedTagging");
+			$triples[] = array(uri($tagging), "tags:taggedResource", uri($post));
+			$triples[] = array(uri($tagging), "tags:associatedTag", literal($tag));
+			$triples[] = array(uri($tagging), "moat:tagMeaning", uri($uri));
+		}
+	}
+	$triples = render_sparql_triples($triples);
+	$query = "INSERT INTO <${post}.rdf> { $triples }";
+	do_query($query);
+	print "The mappings have been successfully updated !";
+}
+
+/////////////////////////////////
+// All the wrappers must inherit from this class
+// And implement the get_uri method that returns an array
+// of URI=>label mappings for the tags, e.g.
+//
+// Array
+// (
+//    [London] => http://dbpedia.org/resource/London
+//    [City of London] => http://dbpedia.org/resource/City_of_London
+// )
+
+abstract class SMOBURIWrapper {
+
+	public function __construct($item) {
+		$this->item = $item;
+	}
+	
+	public function get_uri() {
+		// Needs to be done in each wrapper plug-in
+	}
+
+}
 ?>
