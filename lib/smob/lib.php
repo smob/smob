f25927db8fd9914b7f52835d3a4137cade76720a
milstan <milstan@gmail.com>
2009-10-30 11:47:19 +0000

    opo prefix added
    git-svn-id: http://smob.googlecode.com/svn/trunk@120 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/lib.php
+++ lib/smob/lib.php
@@ -18,6 +18,8 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX dct: <http://purl.org/dc/terms/>
 PREFIX tags: <http://www.holygoat.co.uk/owl/redwood/0.1/tags/>
 PREFIX moat: <http://moat-project.org/ns#>
+PREFIX opo: <http://ggg.milanstankovic.org/opo/ns#>
+PREFIX opo-actions: <http://ggg.milanstankovic.org/opo-actions/ns#>
 
 	$query";
 	$rs = $store->query($query);
