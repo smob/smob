1eabb48edfa7a3deef9e7f6686be696276392247
terraces <apassant@gmail.com>
2009-10-31 19:32:17 +0000

    Remote subscription system
    git-svn-id: http://smob.googlecode.com/svn/trunk@160 e4a05299-234b-0410-97c5-bf58ad63ebc6

---
+++ client/ping.php
@@ -0,0 +1,20 @@
+<?php
+
+parse_str($_SERVER['QUERY_STRING']);
+$u = str_replace('http:/', 'http://', $u);
+
+require_once(dirname(__FILE__)."/../config.php");
+require_once(dirname(__FILE__)."/../lib/smob/lib.php");
+
+if($t == 'follower') {
+	$authority = "http://" . $_SERVER['HTTP_HOST'];
+	$root = $authority . dirname(dirname(dirname($_SERVER['SCRIPT_NAME']))); 
+
+	// Do sanity check over the following URI to ensure it exists, etc.
+	$remote_user = $u;
+	$local_user = user_uri();
+	$follow = "<$remote_user> sioc:follows <$local_user> . ";	
+	$local = "INSERT INTO <$root/data/followers> { $follow }";
+	do_query($local);
+	
+}
