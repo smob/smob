514301470f47f049dc4233f31b10687c29434b49
terraces <apassant@gmail.com>
2010-01-09 21:41:46 +0000

    Added favicon
    git-svn-id: http://smob.googlecode.com/svn/trunk@198 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/SMOBTemplate.php
+++ lib/smob/SMOBTemplate.php
@@ -112,6 +112,7 @@ xml:lang="fr">
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <title>SMOB</title>
   <link rel="stylesheet" type="text/css" href="<?php echo $root; ?>css/style.css" />
+  <link rel="icon" type="image/png" href="<?php echo $root; ?>/img/smob-icon.png" />
   <script type="text/javascript" src="<?php echo $root; ?>js/jquery-1.3.2.min.js"></script>
   <script type="text/javascript" src="<?php echo $root; ?>js/jquery.timers-1.2.js"></script>
   <script type="text/javascript" src="<?php echo $root; ?>js/jquery.bgiframe.min.js"></script>
