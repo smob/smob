00a66b15507c96b6acd53eff2b76b9fbe47c4032
terraces <apassant@gmail.com>
2010-01-09 19:58:36 +0000

    Links to XHTML validator and RDFa extractor
    git-svn-id: http://smob.googlecode.com/svn/trunk@194 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/SMOBTemplate.php
+++ lib/smob/SMOBTemplate.php
@@ -175,7 +175,9 @@ xml:lang="fr">
 </div>
 
 <div id="footer">
-Powered by <a href="http://smob.sioc-project.org/">SMOB</a> thanks to <a href="http://www.w3.org/2001/sw/">Semantic Web</a> technologies and <a href="http://linkeddata.org">Linked Data</a><br/>
+Powered by <a href="http://smob.sioc-project.org/">SMOB</a> thanks to <a href="http://www.w3.org/2001/sw/">Semantic Web</a> and <a href="http://linkeddata.org">Linked Data</a> technologies.
+This page is valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> and <a href="http://www.w3.org/2007/08/pyRdfa/extract?uri=referer">contains RDFa markup</a>.
+<br/>
 </div>
 
 </div>
