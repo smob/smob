d81c3d6e6f78cb7d8ada84356282ac9a3e028b5c
terraces <apassant@gmail.com>
2010-01-11 11:54:40 +0000

    Redirect after authentication
    git-svn-id: http://smob.googlecode.com/svn/trunk@208 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- auth/index.php
+++ auth/index.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once(dirname(__FILE__).'/../lib/smob/SMOBAuth.php'); 
+require_once(dirname(__FILE__).'/../config/config.php'); 
 
 SMOBAuth::grant();
+
+header("Location: $smob_root");
 
 ?>
