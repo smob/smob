c0348b61c60be435f3f35d7b05b4a10b81054139
terraces <apassant@gmail.com>
2010-01-22 14:45:06 +0000

    Ready for v2.0
    git-svn-id: http://smob.googlecode.com/svn/trunk@297 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/SMOBTemplate.php
+++ lib/smob/SMOBTemplate.php
@@ -185,7 +185,7 @@ xml:lang="fr">
 </div>
 
 <div id="footer">
-Powered by <a href="http://smob.sioc-project.org/">SMOB</a> <?php echo $version; ?> thanks to <a href="http://www.w3.org/2001/sw/">Semantic Web</a> and <a href="http://linkeddata.org">Linked Data</a> technologies.<br/>
+Powered by <a href="http://smob.me/">SMOB</a> <?php echo $version; ?> thanks to <a href="http://www.w3.org/2001/sw/">Semantic Web</a> and <a href="http://linkeddata.org">Linked Data</a> technologies.<br/>
 This page is valid <a href="http://validator.w3.org/check?uri=referer">XHTML</a> and <a href="http://www.w3.org/2007/08/pyRdfa/extract?uri=referer">contains RDFa markup</a>.
 <br/>
 </div>
