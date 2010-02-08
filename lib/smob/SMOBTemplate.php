e5d159c81004914af3e27426b01d0751feeb347b
terraces <apassant@gmail.com>
2010-02-08 12:35:16 +0000

    Updated template sidebar
    git-svn-id: http://smob.googlecode.com/svn/trunk@328 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/SMOBTemplate.php
+++ lib/smob/SMOBTemplate.php
@@ -191,20 +191,21 @@ xml:lang="fr">
 <h2>Navigation</h2>
 <ul>
 <li><a href='<?php echo SMOB_ROOT; ?>'>Home</a></li>
-<li><a href='<?php echo SMOB_ROOT; ?>followings'>Followings</a></li>
-<li><a href='<?php echo SMOB_ROOT; ?>followers'>Followers</a></li>
+<li><a href='<?php echo SMOB_ROOT; ?>map'>Map view</a></li>
 <li><a href='<?php echo SMOB_ROOT; ?>sparql'>SPARQL</a></li>
 </ul>
 
-<h2>RSS</h2>
+<h2>People</h2>
 <ul>
-<li><a href='<?php echo SMOB_ROOT; ?>me/rss'>Owner posts</a></li>
+<li><a href='<?php echo SMOB_ROOT; ?>me'>Owner</a> [<a href='<?php echo SMOB_ROOT; ?>me/rss'>RSS</a>]</li>
+<li><a href='<?php echo SMOB_ROOT; ?>followings'>Followings</a></li>
+<li><a href='<?php echo SMOB_ROOT; ?>followers'>Followers</a></li>
+<li><a href='<?php echo SMOB_ROOT; ?>replies'>@replies</a></li>
 </ul>
 
 <h2>Hub owner</h2>
 <ul>
 <li><a href='<?php echo SMOB_ROOT; ?>auth'>Authenticate</a></li>
-<li><a href='<?php echo SMOB_ROOT; ?>replies'>@replies</a></li>
 </ul>
 	
 </div>
