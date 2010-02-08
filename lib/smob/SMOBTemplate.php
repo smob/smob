87baa373a97cb4650a140acf97ae684157cbc32c
terraces <apassant@gmail.com>
2010-02-08 10:50:18 +0000

    Updated map view
    git-svn-id: http://smob.googlecode.com/svn/trunk@325 e4a05299-234b-0410-97c5-bf58ad63ebc6

--- lib/smob/SMOBTemplate.php
+++ lib/smob/SMOBTemplate.php
@@ -102,11 +102,13 @@ $form .= '
 		}
 		global $smob_root;
 		$root = $smob_root ? $smob_root : './';
-		if(!$ismap) {
-			echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" 
- 	"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">';
+		if($ismap) {
+			// GMap hack - FIXME !!
+			echo '<br/>';
 		}
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" 
+ 	"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html
 	xmlns="http://www.w3.org/1999/xhtml" 
 	xmlns:v="urn:schemas-microsoft-com:vml"
