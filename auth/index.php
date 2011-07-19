469633312e36d49d03490bfd3f59a9101e1e759e
janaya <julia.anaya@gmail.com>
2011-07-19 19:41:57 +0100

    Fixed WebID authentication

--- auth/index.php
+++ auth/index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once(dirname(__FILE__).'/../lib/smob/SMOBAuth.php'); 
+require_once(dirname(__FILE__).'/../lib/smob/SMOBStore.php');
+require_once(dirname(__FILE__).'/../lib/smob/SMOBTools.php'); 
 require_once(dirname(__FILE__).'/../config/config.php'); 
 
 SMOBAuth::grant();
--- lib/smob/SMOBAuth.php
+++ lib/smob/SMOBAuth.php
@@ -5,6 +5,7 @@
 	FOAF-SSL methods from : https://foaf.me/testLibAuthentication.php	
 */
 
+
 class SMOBAuth {
 	
 	function check() {
@@ -15,6 +16,7 @@ class SMOBAuth {
 	function grant() {
 		session_start();
 		if(AUTH == 'foafssl') {
+		  error_log('foafssl authentication',0);
 			$foafssl  = SMOBAuth::getAuth();
 			if($foafssl['isAuthenticated']) {
 				$_SESSION['grant'] = true;				
@@ -24,6 +26,7 @@ class SMOBAuth {
 		} else {
 			$_SESSION['grant'] = true;				
 		}
+		error_log($foafssl['authDiagnostic'],0);
 	}
 	
 	/* Function to return the modulus and exponent of the supplied Client SSL Page */
@@ -31,6 +34,7 @@ class SMOBAuth {
 	{
 		if ($_SERVER[SSL_CLIENT_CERT])
 		{
+		  error_log('got a certificate',0);
 			$pub_key = openssl_pkey_get_public($_SERVER[SSL_CLIENT_CERT]);
 			$key_data = openssl_pkey_get_details($pub_key);
 
@@ -53,6 +57,7 @@ class SMOBAuth {
 
 			return( array( 'modulus'=>$modulus[3], 'exponent'=>$exponent[3] ) );
 		}
+		error_log('no certificate',0);
 	}
 
 	/* Returns an array holding the subjectAltName of the supplied SSL Client Certificate */
@@ -109,15 +114,30 @@ class SMOBAuth {
 					rsa:modulus [ cert:hex ?mod ] ;
 					rsa:public_exponent [ cert:decimal ?exp ] .
 			  }';
-			if ($rows = $store->query($q, 'rows')) 
-			{
-				foreach ($rows as $row) 
-				{
-					$modulus =  cleanhex($row['mod']);
-					$exponent = cleanhex($row['exp']);
-				}
-			}
-
+			// for some reason the previous query doesn't work in ARC
+			// sig is not needed, replazed by bnode
+			// eliminate #me as it should be in the certificate URI
+			$q = '
+			  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+			  PREFIX rsa: <http://www.w3.org/ns/auth/rsa#> 
+			  PREFIX cert: <http://www.w3.org/ns/auth/cert#>
+			  PREFIX foaf: <http://xmlns.com/foaf/0.1/> .
+			  SELECT ?mod ?exp  WHERE {
+				[] a rsa:RSAPublicKey;
+          cert:identity <'.$uri.'#me>;
+					rsa:modulus ?mod ;
+					rsa:public_exponent ?exp .
+			  }';
+			$res = SMOBStore::query($q);
+			if ($res) {
+			  // TODO: support several keys for webid uri?
+		    $modulus =  SMOBAuth::cleanhex($res[0]['mod']);
+		    $exponent =  SMOBAuth::cleanhex($res[0]['exp']);
+		    error_log('modulus: ',0);
+		    error_log($modulus, 0);
+		    error_log('exponent: ',0);
+		    error_log($exponent, 0);
+      }
 			if ($modulus && $exponent)
 				return ( array( 'modulus'=>$modulus, 'exponent'=>dechex($exponent) ) );
 		}
@@ -126,7 +146,7 @@ class SMOBAuth {
 	/* Function to compare two supplied RSA keys */
 	function equal_rsa_keys($key1, $key2)
 	{
-		if ( $key1 && $key2 && ($key1[modulus] == $key2[modulus]) && ($key1[exponent] == $key2[exponent]) )
+		if ( $key1 && $key2 && ($key1['modulus'] == $key2['modulus']) && ($key1['exponent'] == $key2['exponent']) )
 			return TRUE;
 
 		return FALSE;
@@ -140,23 +160,29 @@ class SMOBAuth {
 		if (!$_SERVER[SSL_CLIENT_CERT])
 			return ( array( 'isAuthenticated'=>0 , 'authDiagnostic'=>'No client certificate supplied') );
 
-		$certrsakey = openssl_pkey_get_public_hex();
+    error_log('certificate:',0);
+    error_log($_SERVER[SSL_CLIENT_CERT], 0);
+		$certrsakey = SMOBAuth::openssl_pkey_get_public_hex();
 
 		if (!$certrsakey)
 			return ( array( 'isAuthenticated'=>0 , 'authDiagnostic'=>'No RSA Key in the supplied client certificate') );
+    error_log('rsa key:',0);
+    error_log($certrsakey['modulus'],0);
 
 		$result = array('certRSAKey'=>$certrsakey);
 
-		$san     = openssl_get_subjectAltName();
+		$san     = SMOBAuth::openssl_get_subjectAltName();
 		$foafuri = $san[URI];
-
+    error_log('foaf ur:',0);
+    error_log($foafuri,0);
+    
 		$result = array_merge($result, array('subjectAltName'=>$foafuri));
 
-		$foafrsakey = get_foaf_rsakey($foafuri);
+		$foafrsakey = SMOBAuth::get_foaf_rsakey($foafuri);
 
 		$result = array_merge($result, array('subjectAltNameRSAKey'=>$foafrsakey));
 
-		if ( equal_rsa_keys($certrsakey, $foafrsakey) )
+		if ( SMOBAuth::equal_rsa_keys($certrsakey, $foafrsakey) )
 			$result = array_merge($result, array( 'isAuthenticated'=>1,  'authDiagnostic'=>'Client Certificate RSAkey matches SAN RSAkey'));
 		else
 			$result = array_merge($result, array( 'isAuthenticated'=>0,  'authDiagnostic'=>'Client Certificate RSAkey does not match SAN RSAkey'));
